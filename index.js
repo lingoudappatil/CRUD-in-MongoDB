cc04586bf011137688cb60f9dce848cae6823eea
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-06-28 22:28:54 +0530

    index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose= require("mongoose");
 const app = express();
 const port = 8000;
 
+
 //connection
 mongoose.connect('mongodb://127.0.0.1:27017/db_demo1')
 .then(()=> console.log("Mongobd is connected"))
