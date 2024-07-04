6f76065a963113a69bb789bd1b34e09d67c4f78c
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-07-04 08:14:10 +0530

    dbs_con.js

--- dbs_con.js
+++ dbs_con.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/db_demo1', {
+        await mongoose.connect('mongodb://127.0.0.1:27017/db_demo1', { 
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
